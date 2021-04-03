5b2830e80dcbd83f7a8f243e30eb65790053ed9f
iftahnaf <iftahnaf@gmail.com>
2021-04-03 16:08:32 +0300

    version 1.0 - system working

--- src/main.cpp
+++ src/main.cpp
@@ -12,33 +12,34 @@ PWM -> D9
 #include <ros/time.h>
 #include <sensor_msgs/Range.h>
 #include "readSonar.h"
-#include <std_msgs/String.h>
 #include <Servo.h>
+#include <std_msgs/UInt16.h>
 
 #define trigPin 2
 #define echoPin 4
 #define servoPin 9
+#define servoPower 13
 
 ros::NodeHandle  nh;
 
 sensor_msgs::Range range_msg;
 ros::Publisher pub_range( "/ultrasound", &range_msg);
 sonar ultrasonic;
-std_msgs::String str_msg;
-ros::Publisher chatter("/test", &str_msg);
+std_msgs::UInt16 servoPos;
+ros::Publisher pub_pos("/servo", &servoPos);
 Servo myservo;
 
 long duration;
 float distance;
-char hello[13] = "hello world!";
 char frameid[] = "/ultrasound";
-int pos;
+int pos = 60;
+int flag = 0;
 
 void setup()
 {
   nh.initNode();
   nh.advertise(pub_range);
-  nh.advertise(chatter);
+  nh.advertise(pub_pos);
   
   range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
   range_msg.header.frame_id =  frameid;
@@ -48,16 +49,20 @@ void setup()
     
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(servoPower,OUTPUT);
+  digitalWrite(servoPower, HIGH);
 
   myservo.attach(servoPin);
 }
 
 void loop()
 {
+  myservo.write(pos);
+
   duration = ultrasonic.timeTravel(echoPin, trigPin);
 
-  str_msg.data = hello;
-  chatter.publish( &str_msg );
+  servoPos.data = pos;
+  pub_pos.publish(&servoPos);
 
   if (duration < 100000)
   {
@@ -66,7 +71,20 @@ void loop()
     range_msg.header.stamp = nh.now();
     pub_range.publish(&range_msg);
   }
-  delay(100); 
+
+  delay(10); 
+
+  if ((pos <= 135) && flag == 0){
+    pos=pos+2;
+  }
+  else{
+    flag =1;
+    pos=pos-2;
+  }
+  if (pos < 60){
+    flag = 0;
+    pos = 60;
+  }
 
   nh.spinOnce();
 }
