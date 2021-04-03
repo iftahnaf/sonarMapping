9a502242ecbda9625f79aee9b132399536cf8d24
iftahnaf <iftahnaf@gmail.com>
2021-04-03 22:17:43 +0300

    creating controlServo and publishMeasuredDistance funcitons

--- src/main.cpp
+++ src/main.cpp
@@ -20,50 +20,44 @@ PWM -> D9
 #define servoPin 9
 #define servoPower 13
 
-ros::NodeHandle  nh;
+ros::NodeHandle nh;
 
 sensor_msgs::Range range_msg;
-ros::Publisher pub_range( "/ultrasound", &range_msg);
+ros::Publisher pub_range("/ultrasound", &range_msg);
 sonar ultrasonic;
 std_msgs::UInt16 servoPos;
 ros::Publisher pub_pos("/servo", &servoPos);
 Servo myservo;
 
-long duration;
-float distance;
 char frameid[] = "/ultrasound";
 int pos = 60;
 int flag = 0;
+int minAngle = 60;
+int maxAngle = 135;
 
-void setup()
+void controlServo(int &pos, int &flag, int minAngle, int maxAngle)
 {
-  nh.initNode();
-  nh.advertise(pub_range);
-  nh.advertise(pub_pos);
-  
-  range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
-  range_msg.header.frame_id =  frameid;
-  range_msg.field_of_view = 60.0;  // fake
-  range_msg.min_range = 0.0;
-  range_msg.max_range = 1200.00;
-    
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(servoPower,OUTPUT);
-  digitalWrite(servoPower, HIGH);
-
-  myservo.attach(servoPin);
+  if ((pos <= 135) && flag == 0)
+  {
+    pos = pos + 2;
+  }
+  else
+  {
+    flag = 1;
+    pos = pos - 2;
+  }
+  if (pos < 60)
+  {
+    flag = 0;
+    pos = 60;
+  }
 }
 
-void loop()
+void publishMeasuredDistance()
 {
-  myservo.write(pos);
-
+  long duration;
+  float distance;
   duration = ultrasonic.timeTravel(echoPin, trigPin);
-
-  servoPos.data = pos;
-  pub_pos.publish(&servoPos);
-
   if (duration < 100000)
   {
     distance = ultrasonic.microSecondsToCentimeters(duration);
@@ -71,20 +65,36 @@ void loop()
     range_msg.header.stamp = nh.now();
     pub_range.publish(&range_msg);
   }
+}
 
-  delay(10); 
+void setup()
+{
+  nh.initNode();
+  nh.advertise(pub_range);
+  nh.advertise(pub_pos);
 
-  if ((pos <= 135) && flag == 0){
-    pos=pos+2;
-  }
-  else{
-    flag =1;
-    pos=pos-2;
-  }
-  if (pos < 60){
-    flag = 0;
-    pos = 60;
-  }
+  range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg.header.frame_id = frameid;
+  range_msg.field_of_view = 60.0; // fake
+  range_msg.min_range = 0.0;
+  range_msg.max_range = 1200.00;
 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(servoPower, OUTPUT);
+  digitalWrite(servoPower, HIGH);
+
+  myservo.attach(servoPin);
+}
+
+void loop()
+{
+  controlServo(pos, flag, minAngle, maxAngle);
+  myservo.write(pos);
+  servoPos.data = pos;
+  pub_pos.publish(&servoPos);
+  publishMeasuredDistance();
+  delay(10);
   nh.spinOnce();
 }
+
